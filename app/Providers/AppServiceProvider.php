35a74c7d3179c436f7632f3d49e0c1b998228e74
MetaGrenade <fragzone.caleb@gmail.com>
2025-04-19 15:43:23 -0600

    Fix Github Actions CI Error
    The CI failure is happening because in our GitHub Actions run there is no real `.env` file, so Laravel falls back to its default database connection (which in our `config/database.php` and is set to sqlite), and then our unconditional in AppServiceProvider::boot() runs immediately—even against the SQLite connection—and blows up when it tries to run an unsupported SQL statement on a missing `database.sqlite` file.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // for every new MySQL connection:
-        DB::statement("SET time_zone = '+00:00'");
+        // Only set MySQL’s time zone when you’re actually using MySQL
+        // When GitHub Actions run there is no real .env file, so Laravel falls back to its default database connection
+        // Which is set in config/database.php :19 'default' => env('DB_CONNECTION', 'sqlite'),
+        if (DB::getDriverName() === 'mysql') {
+            DB::statement("SET time_zone = '+00:00'");
+        }
     }
 }
