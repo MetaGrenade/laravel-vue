e9547284de0ad9fa974e7d4db8534a57da3250bb
Caleb Harrod <fragzone.caleb@gmail.com>
2025-09-30 14:04:47 -0600

    Ensure email verification middleware honours system setting

--- app/Http/Middleware/EnsureEmailIsVerifiedIfRequired.php
+++ app/Http/Middleware/EnsureEmailIsVerifiedIfRequired.php
@@ -4,12 +4,11 @@ namespace App\Http\Middleware;
 
 use App\Support\EmailVerification;
 use Closure;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Auth\Middleware\EnsureEmailIsVerified as BaseMiddleware;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 use Symfony\Component\HttpFoundation\Response;
 
-class EnsureEmailIsVerifiedIfRequired
+class EnsureEmailIsVerifiedIfRequired extends BaseMiddleware
 {
     /**
      * Handle an incoming request.
@@ -20,54 +19,6 @@ class EnsureEmailIsVerifiedIfRequired
             return $next($request);
         }
 
-        $user = $request->user();
-
-        if (! $user || ($this->requiresVerification($user) && ! $this->hasVerifiedEmail($user))) {
-            return $request->expectsJson()
-                ? abort(Response::HTTP_FORBIDDEN, 'Your email address is not verified.')
-                : Redirect::guest($this->redirectTo($request, $redirectToRoute));
-        }
-
-        return $next($request);
-    }
-
-    /**
-     * Determine if the given user should be considered for verification.
-     */
-    protected function requiresVerification(object $user): bool
-    {
-        if ($user instanceof MustVerifyEmail) {
-            return true;
-        }
-
-        return property_exists($user, 'email_verified_at') || method_exists($user, 'getAttribute');
-    }
-
-    /**
-     * Determine if the given user has a verified email address.
-     */
-    protected function hasVerifiedEmail(object $user): bool
-    {
-        if (method_exists($user, 'hasVerifiedEmail')) {
-            return (bool) $user->hasVerifiedEmail();
-        }
-
-        $emailVerifiedAt = null;
-
-        if (method_exists($user, 'getAttribute')) {
-            $emailVerifiedAt = $user->getAttribute('email_verified_at');
-        } elseif (property_exists($user, 'email_verified_at')) {
-            $emailVerifiedAt = $user->email_verified_at;
-        }
-
-        return ! is_null($emailVerifiedAt);
-    }
-
-    /**
-     * Get the path the user should be redirected to when verification is required.
-     */
-    protected function redirectTo(Request $request, ?string $route): string
-    {
-        return $route ? route($route) : route('verification.notice');
+        return parent::handle($request, $next, $redirectToRoute);
     }
 }
