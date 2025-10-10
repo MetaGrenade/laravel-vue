67c2488702d83fd91432f210893ddd775ef8b41a
Caleb Harrod <fragzone.caleb@gmail.com>
2025-10-10 01:30:24 -0600

    Fix broadcasting route registration (#148)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,9 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withBroadcasting(function () {
-        require __DIR__.'/../routes/channels.php';
-    })
+    ->withBroadcasting(__DIR__.'/../routes/channels.php')
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance']);
 
