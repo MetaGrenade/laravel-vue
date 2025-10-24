89be96d19afd3746322198beef3e9825863e625f
Caleb Harrod <fragzone.caleb@gmail.com>
2025-10-24 13:23:00 -0600

    Fix ForumIndexCache observer method signatures

--- app/Observers/ForumIndexCacheObserver.php
+++ app/Observers/ForumIndexCacheObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Support\Forum\ForumIndexCache;
+use Illuminate\Database\Eloquent\Model;
 
 class ForumIndexCacheObserver
 {
@@ -10,22 +11,22 @@ class ForumIndexCacheObserver
     {
     }
 
-    public function saved(): void
+    public function saved(Model $model): void
     {
         $this->cache->clear();
     }
 
-    public function deleted(): void
+    public function deleted(Model $model): void
     {
         $this->cache->clear();
     }
 
-    public function restored(): void
+    public function restored(Model $model): void
     {
         $this->cache->clear();
     }
 
-    public function forceDeleted(): void
+    public function forceDeleted(Model $model): void
     {
         $this->cache->clear();
     }
